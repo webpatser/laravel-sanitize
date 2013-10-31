0e264031b7ca5890daaba920713c43c964a8ff0d
Christoph Kempen <christoph@downsized.nl>
2013-10-31 02:43:28 +0100

    Adding start for UnitTests and travisCI

---
+++ tests/Test.php
@@ -0,0 +1,16 @@
+<?php
+
+use Webpatser\Sanitize;
+
+class SanitizeTest extends \PHPUnit_Framework_TestCase {
+
+    public function test_clean()
+    {
+        $url = Sanitize::string('some_string');
+        $this->assertEquals('some_string', $url);
+
+        $url = Sanitize::string('Some String');
+        $this->assertEquals('some-string', $url);
+    }
+}
+ 
